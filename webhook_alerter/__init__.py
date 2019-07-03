886e696314049eeeec42a5c06cb40ba0be92bbe2
Ben Walker <ben.walker@scivisum.co.uk>
2019-07-03 15:45:02 +0100

    Scan the views and alerters separately

--- webhook_alerter/__init__.py
+++ webhook_alerter/__init__.py
@@ -1,10 +1,14 @@
+import venusian
 from pyramid.config import Configurator
+from webhook_alerter import views, alerters
+from webhook_alerter.alerters.alerters import BaseAlerter
 
 
-def main(global_config, **settings):
+def main(_, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
     config.add_route('alerter', '/{config}')
-    config.scan()
+    config.scan(views)
+    venusian.Scanner().scan(alerters, categories=['alerters'])
     return config.make_wsgi_app()
