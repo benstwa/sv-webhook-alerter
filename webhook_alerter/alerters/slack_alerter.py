006efad993e8a3399cb1ce95129fa7f4894009bc
Ben Walker <ben.walker@thinktribe.com>
2020-06-12 11:40:24 +0100

    Include the URL in the error message

--- webhook_alerter/alerters/slack_alerter.py
+++ webhook_alerter/alerters/slack_alerter.py
@@ -1,6 +1,7 @@
 import time
 import slacker
 import logging
+import requests
 from webhook_alerter.alerters.alerters import BaseAlerter, alerter_config
 
 
@@ -20,6 +21,18 @@ class SlackWebhookAlerter(BaseAlerter):
             self.slack_params = {}
         self.slack = slacker.IncomingWebhook(self.url)
 
+    def step_string(self, step):
+        return " - %s in step %s: %s" % (step['message'], step['step']['number'], step['step']['name'])
+
+    def build_error_string(self, data: dict) -> str:
+        cause_string = ""
+        errors = data['payload']['sample']['causes']['errors']
+        for i, step in enumerate(errors['byStep']):
+            if i:
+                cause_string += "\n\n"
+            cause_string += self.step_string(step)
+        return cause_string
+
     def build_message(self, data: dict) -> dict:
         state = data['payload']['state']
 
@@ -27,9 +40,6 @@ class SlackWebhookAlerter(BaseAlerter):
 
         if state != 'RECOVERED':
             cause_string = ""
-
-            def step_string(step):
-                return " - %s in step %s: %s" % (step['message'], step['step']['number'], step['step']['name'])
 
             causes = data['payload']['sample']['causes']
             if 'slows' in causes:
@@ -39,13 +49,11 @@ class SlackWebhookAlerter(BaseAlerter):
                     cause_string += "\n\n"
                 if 'byStep' in slows:
                     for step in slows['byStep']:
-                        cause_string += step_string(step)
+                        cause_string += self.step_string(step)
                         cause_string += "\n\n"
             if 'errors' in causes:
-                errors = causes['errors']
-                for step in errors['byStep']:
-                    cause_string += step_string(step)
-                    cause_string += "\n\n"
+                cause_string += self.build_error_string(data)
+
             alert_dict['causes'] = cause_string[:-2]
 
         if 'causedBy' in data['payload']:
@@ -90,3 +98,49 @@ class SlackWebhookAlerter(BaseAlerter):
     def send_alert(self, data):
         message = self.build_message(data)
         self.send(lambda: self.slack.post(message), self.callback)
+
+
+@alerter_config
+class SlackWebhookAlerterWithURL(SlackWebhookAlerter):
+
+    def __init__(self, *args, **kwargs):
+        self.api_key = kwargs.pop('apikey')
+        SlackWebhookAlerter.__init__(self, *args, **kwargs)
+
+    def build_error_string(self, data: dict) -> str:
+        uj_id = data['payload']['uj']['id']
+        sample_time = data['payload']['time']
+        query = """
+          userJourney(ujId: %s) {
+            sample(sampleTime: "%s", backup: false) {
+              errorCauses{
+                stepNumber
+                message
+                status
+                url
+              }
+            }
+          }
+        """ % (uj_id, sample_time)
+        response = requests.post(
+            "https://portal.thinktribe.com/api/private",
+            headers={"Authorization": "Bearer {}".format(self.api_key)},
+            json={"query": "{ %s }" % query},
+            timeout=60
+        ).json()
+
+        cause_string = ""
+        errors = data['payload']['sample']['causes']['errors']
+        for i, step in enumerate(errors['byStep']):
+            if i:
+                cause_string += "\n\n"
+            number = step['step']['number']
+            name = step['step']['name']
+            for error in response['data']['userJourney']['sample']['errorCauses']:
+                if error['status'] == 'SLOW':
+                    continue
+                if error['stepNumber'] == number:
+                    cause_string += " - %s in component: %s in step %s: %s" % (
+                        error['message'], error['url'], number, name
+                    )
+        return cause_string
